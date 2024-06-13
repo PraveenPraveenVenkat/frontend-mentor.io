74626a1d371217b77bc6416604f347d27e12e737
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-06-13 10:47:51 +0530

    Contains Duplicate in Array

---
+++ Javascript/index.js
@@ -0,0 +1,33 @@
+ // Named function
+ function RocketToMars() {
+    return 'BOOM!';
+  }
+  
+  // Anonymous function
+  const rocketToMars = function() {
+    console.log ("BOOM!");
+  }
+  rocketToMars("function");
+  
+   
+   // Defining the function:
+  function Sum(num1, num2) {
+  }
+  // Calling the function:
+  Sum(3, 6); // 9
+  console.log(Sum(3, 6)); // Outputs: 9
+  
+  
+  const printHello = () => { 
+    console.log('hello'); 
+  }; 
+  printHello(); // => hello
+  
+  const sum = (param1, param2) => { 
+    return param1 + param2; 
+  }; 
+  console.log(sum(2,5)); // => 7 
+  
+  
+  
+  
