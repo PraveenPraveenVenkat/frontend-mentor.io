729b7a38391394176d18ad0b5baec7d1759e506f
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-06-13 11:09:41 +0530

    JSarray

---
+++ Javascript/FindTheArray.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+/*
+export const containsDuplicate = function (nums) {
+
+    function nums(){
+   
+    if(containsDuplicate =true){
+        return true;
+    }elseif (containsDuplicate !=true){
+        return false;
+    }
+ } 
+ nums();*/
+
+ /*
+ // Function implementation
+export const containsDuplicate = function(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    }
+    return false;
+};
+
+// Example usage
+const array1 = [1, 2, 3, 4];
+const array2 = [1, 2, 3, 1];
+
+console.log(containsDuplicate(array1)); // Outputs: false
+console.log(containsDuplicate(array2)); // Outputs: true
+
+}
+*/
+// Corrected code
+export const containsDuplicate = function(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    } 
+    return false;
+};
+
+// Ensure there are no missing statements or declarations below this point
+// Corrected code
+
+
+
