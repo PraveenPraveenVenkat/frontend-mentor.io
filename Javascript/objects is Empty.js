56334bdee049a46c22faae924e0a88c0956d4315
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-08-21 23:21:36 +0530

    Is The Object Is Empty

---
+++ Javascript/objects is Empty.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+
+var isEmpty = function(obj) {
+    return  Object.entries(obj).length == 0
+}; 
+// if (obj === key){
+//  return(fase);
+// }  else if (Object.keys(obj).length === 0) {
+    
+//     // Your code here
+// }
+
+//     return(true);
+// };
+
