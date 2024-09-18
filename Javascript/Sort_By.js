84723f6502343aefb280586a5cb63ff54807dade
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-09-18 21:08:34 +0530

    SortBy

---
+++ Javascript/Sort_By.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+
+
+    return arr.sort((a, b) => fn(a) - fn(b));
+    // return arr.sort();
+    // conole.log(arr);
+};
